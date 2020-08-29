3d0a414993ada98a1d9d09c019644d3dc431c6c7
John <dongyongkim8@gmail.com>
2020-08-29 12:07:34 -0400

    changed file reader method to utilize BufferedReader. Added some logic in driver code. Added print statements for user

--- src/bigramfrequencycounter/BigramFrequencyCounter.java
+++ src/bigramfrequencycounter/BigramFrequencyCounter.java
@@ -21,17 +21,36 @@ public class BigramFrequencyCounter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        //checks if the controlInterface successfully performed all opperations
+        boolean controlInterface;
+        
+        //the instance of the Control class
         Control mainControler = new Control();
-        //mainControler.readInFileToArrayList1("bigram.txt");
-        mainControler.controlInterface("bigram.txt");
-        Map<String, Integer> sortedMap = new HashMap<>();
-        HashMap<String, Integer> unsortedMap = mainControler.getMap();
-        sortedMap = mainControler.sortByValue(unsortedMap);
-        for (Map.Entry<String, Integer> en : sortedMap.entrySet()) { 
-            System.out.println("Key = " + en.getKey() +  
-                          ", Value = " + en.getValue()); 
-        } 
-        //System.out.println(tempInt.toString());
+
+        //calls the controlInterface method and passes in .txt file path
+        controlInterface = mainControler.controlInterface("bigram.txt");
+        
+        //if controlInterace performs all actions sucessfully...
+        if (controlInterface) {
+            System.out.println("Counting bigram frequency...\n");
+            
+            //will store sorted bigrams
+            Map<String, Integer> sortedMap = new HashMap<>(); 
+            
+            //will store the unsorted bigrams loaded in from the Control class
+            HashMap<String, Integer> unsortedMap = mainControler.getMap();
+            sortedMap = mainControler.sortByValue(unsortedMap);
+            int x = 0;
+            for (Map.Entry<String, Integer> en : sortedMap.entrySet()) {
+                System.out.println("Bigram: \"" + en.getKey()
+                        + "\", Frequency of occurances: " + en.getValue());
+                if (x == 20) {
+                    break;
+                }
+                x++;
+            }
+        }
     }
- 
+
 }
--- src/bigramfrequencycounter/Control.java
+++ src/bigramfrequencycounter/Control.java
@@ -5,7 +5,10 @@
  */
 package bigramfrequencycounter;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +19,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 /**
  *
@@ -24,77 +26,102 @@ import java.util.Scanner;
  */
 public class Control {
 
-    //class scanner object
-    private Scanner scanner;
+    //class BufferedReader object to load file
+    private BufferedReader bufferedReader = null;
     //class ArrayList that hold initial file
     private final ArrayList<String> InitialList = new ArrayList<>();
     //class ArrayList will hold all the bigrams
     private final ArrayList<String> bigramList = new ArrayList<>();
     //class HashMap will store all the bigrams and their occurence frequency
     private final HashMap<String, Integer> frequency = new HashMap<>();
-    
+
 //------------------------Access Method-----------------------------------------
     
-    public HashMap<String, Integer> getMap(){
+    /**
+     * 
+     * @return 
+     */
+    public HashMap<String, Integer> getMap() {
         return frequency;
     }
-    
+
 //------------------------------------------------------------------------------
     
-    public boolean controlInterface(String fileName){
-        readInFileToArrayList1(fileName);
-        scanner.close();
-        if(!InitialList.isEmpty()){
+    /**
+     * Is the main interaction method that can be called outside the class. This
+     * method will call all of the internal methods to perform the load, sort, and
+     * counting operations.
+     * @param fileName the relative or absolute path of the file as a String which
+     * is used to pass in as an argument for the bufferedReaderToArrayList method.
+     * @return true if all actions are successful
+     */
+    public boolean controlInterface(String fileName) {
+        bufferedReaderToArrayList(fileName);
+        if (!InitialList.isEmpty()) {
+            System.out.println("Loading file... \n");
             createBigramArrayList(InitialList);
             InitialList.clear();//clear unused ArrayList
-        }else{
+            System.out.println("Loading complete\n");
+        } else {
             System.out.println("File is empty");
             return false;
         }
-        if(!bigramList.isEmpty()){
+        if (!bigramList.isEmpty()) {
+            System.out.println("Creating bigram list... \n");
             frequencyCounter(bigramList);
             bigramList.clear();//clear unused ArrayList
-        }else{
+            System.out.println("bigram list compiled \n");
+        } else {
             System.out.println("This file is incompatable becuase it does not contain any bigrams");
             return false;
         }
-        
         return true;
     }
-    
+
 //------------------------------------------------------------------------------
 
     /**
-     * Uses the class scanner object to read in the file and load it into a
-     * ArrayList of type String. Each listElement of the ArrayList is an individual
-     * String from the file
-     * @param fileName the Path of the file as a String
-     * @return The loaded ArrayList
+     * Utilizes the BufferedReader class to read large text file. The reader will
+     * take in the file and produce Strings of each line. The .split method is 
+     * used to separate each line into individual words. These words are then
+     * added to the InitialList ArrayList.
+     * @param fileName the relative or absolute path of the file as a String
      */
-    public void readInFileToArrayList1(String fileName) {
+    private void bufferedReaderToArrayList(String fileName) {
         try {
-            scanner = new Scanner(new File(fileName));
-            String temp = "";
-            int i = 0;
-            while (scanner.hasNext()) {
-                //System.out.println(temp = scanner.next());
-                InitialList.add(scanner.next());
-                i++;
+            bufferedReader = new BufferedReader(new FileReader(new File(fileName)));
+            String available;
+            while ((available = bufferedReader.readLine()) != null) {
+                String[] lineArray = available.split("\\s+");
+                for (int i = 0; i < lineArray.length; i++) {
+                    InitialList.add(lineArray[i]);
+                }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("file not found");
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-    }//readInFileToArrayList
+    }//bufferedReaderToArrayList
 
     /**
-     *Goes through the class ArrayList that is storing the file and parses the 
+     * Goes through the class ArrayList that is storing the file and parses the
      * ArrayList to create bigrams (two word sequences). the firstWord gets the
-     * value of the first word of the first bigram. The secondWord gets the value
-     * of the second word of the bigram by passing the offset into as an argument
-     * in .get. these words are concatenated together to yeld a bigram. To get the 
-     * second bigram the firstWord is set to the secondWord while the second is
-     * set to get offset + 1 from the initialList.
-     * @param inintialList should be a text file converted to an ArrayList cast 
+     * value of the first word of the first bigram. The secondWord gets the
+     * value of the second word of the bigram by passing the offset into as an
+     * argument in .get. these words are concatenated together to yeld a bigram.
+     * To get the second bigram the firstWord is set to the secondWord while the
+     * second is set to get offset + 1 from the initialList.
+     *
+     * @param inintialList should be a text file converted to an ArrayList cast
      * to String type
      */
     public void createBigramArrayList(ArrayList<String> inintialList) {
@@ -103,7 +130,7 @@ public class Control {
         String bigram = "";
         int offset = 0;//will set the word to be the second word in bigram
         int i = 0;
-        while( i < inintialList.size()-2) { //minus 2 because 1 less than size and 1 less for the offset
+        while (i < inintialList.size() - 2) { //minus 2 because 1 less than size and 1 less for the offset
             offset = i + 1;
             firstWord = inintialList.get(i);
             secondWord = inintialList.get(offset);
@@ -113,30 +140,31 @@ public class Control {
             secondWord = inintialList.get(offset + 1);
             bigram = firstWord.concat(" " + secondWord);
             bigramList.add(bigram);
-            i = offset + 1;          
+            i = offset + 1;
         }
-    }
+    }//createBigramArrayList
 
     /**
-     * Reads the data from an ArrayList of String type, and will count the number
-     * of occurences of each ArrayList item. Creates a boolean array to keep track
-     * of which items have been checked. Uses nested loops to check each element
-     * against the previously compared elements. Count will increment if match is
-     * found. The ArrayList item String will be added to the key of a HashMap and
-     * the count will be added as the value.
+     * Reads the data from an ArrayList of String type, and will count the
+     * number of occurences of each ArrayList item. Creates a boolean array to
+     * keep track of which items have been checked. Uses nested loops to check
+     * each element against the previously compared elements. Count will
+     * increment if match is found. The ArrayList item String will be added to
+     * the key of a HashMap and the count will be added as the value.
+     *
      * @param bigramList An ArrayList of bigram Strings
      */
-    public void frequencyCounter(ArrayList<String> bigramList){
+    public void frequencyCounter(ArrayList<String> bigramList) {
         int size = bigramList.size();
         boolean visited[] = new boolean[size];
-        Arrays.fill(visited, false);    
-        for(int i = 0; i < size; i++){
-            if(visited[i] == true){
+        Arrays.fill(visited, false);
+        for (int i = 0; i < size; i++) {
+            if (visited[i] == true) {
                 continue;
             }
             int count = 1;
-            for(int j = i + 1; j < size; j++){
-                if(bigramList.get(i).equalsIgnoreCase(bigramList.get(j))){
+            for (int j = i + 1; j < size; j++) {
+                if (bigramList.get(i).equalsIgnoreCase(bigramList.get(j))) {
                     visited[j] = true;
                     count++;
                 }
@@ -144,31 +172,32 @@ public class Control {
             frequency.put(bigramList.get(i), count);
         }
     }//frequencyCounter
-    
+
     /**
      * Will take in unsorted HashMap and sort based off of values. The argument
-     * data will be put into a list. The list is sorted by calling the sort method
-     * from Collections which in turn calls a lambda expression that compares the
-     * different list items. The sorted List entries are added to the new HashMap
+     * data will be put into a list. The list is sorted by calling the sort
+     * method from Collections which in turn calls a lambda expression that
+     * compares the different list items. The sorted List entries are added to
+     * the new HashMap
+     *
      * @param unsortedMap unsorted map data must be in String, Integer format
      * @return a sortedMap with values going in descending order
      */
-    public HashMap<String, Integer> sortByValue(HashMap<String, Integer> unsortedMap) 
-    { 
+    public HashMap<String, Integer> sortByValue(HashMap<String, Integer> unsortedMap) {
         //Takes unsorted data and creates list
-        List<Map.Entry<String, Integer> > list = 
-               new LinkedList<>(unsortedMap.entrySet()); 
+        List<Map.Entry<String, Integer>> list
+                = new LinkedList<>(unsortedMap.entrySet());
         //calls the sort on list by calling a lambda expression that calls the compareTo method
-        Collections.sort(list, (Map.Entry<String, Integer> FirstElement, 
-                Map.Entry<String, Integer> secondElement) -> 
-                (secondElement.getValue()).compareTo(FirstElement.getValue())); 
+        Collections.sort(list, (Map.Entry<String, Integer> FirstElement,
+                Map.Entry<String, Integer> secondElement)
+                -> (secondElement.getValue()).compareTo(FirstElement.getValue()));
         //creates the return HashMap, takes list items, and puts them in sortedMap
-        HashMap<String, Integer> sortedMap = new LinkedHashMap<>(); 
-        for (Map.Entry<String, Integer> listElement : list) { 
-            sortedMap.put(listElement.getKey(), listElement.getValue()); 
-        } 
+        HashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> listElement : list) {
+            sortedMap.put(listElement.getKey(), listElement.getValue());
+        }
         frequency.clear();//clear unused class HashMap
-        return sortedMap; 
+        return sortedMap;
     }//sortByValue
 
 }//Control
